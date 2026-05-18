7d9ce0522425f25a05ede4b55cc2d2c3d1cc68dc
Daikinetherite2 <115567108+Daikinetherite2@users.noreply.github.com>
2026-05-19 03:02:41 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
-function copyIP() {
-    const ip = "play.afterfall.com";
+// FIREBASE CONFIG (ISI DARI PROJECT KAMU)
+const firebaseConfig = {
+  apiKey: "XXX",
+  authDomain: "XXX",
+  databaseURL: "XXX",
+  projectId: "XXX",
+};
 
-    navigator.clipboard.writeText(ip).then(() => {
-        alert("IP Copied: " + ip);
-    });
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
+const messagesRef = db.ref("messages");
+
+// realtime listener
+messagesRef.on("child_added", (data) => {
+  const msg = data.val();
+  const div = document.createElement("div");
+  div.classList.add("msg");
+  div.innerText = `${msg.user}: ${msg.text}`;
+  document.getElementById("messages").appendChild(div);
+});
+
+function sendMessage() {
+  const user = document.getElementById("username").value || "anon";
+  const text = document.getElementById("message").value;
+
+  if (!text) return;
+
+  messagesRef.push({
+    user,
+    text
+  });
+
+  document.getElementById("message").value = "";
 }
